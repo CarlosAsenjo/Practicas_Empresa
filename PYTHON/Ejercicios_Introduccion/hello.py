b69b4e324701605457bea3b71e71e03dabbb5ab6
CarlosAsenjo <carlosasenjo.daw@gmail.com>
2020-03-31 08:30:06 +0200

    hello.py

---
+++ PYTHON/Ejercicios_Introduccion/hello.py
@@ -0,0 +1,18 @@
+from flask import Flask, escape, request
+app = Flask(__name__)
+@app.route('/')
+
+def hello():
+    nombre = request.args.get("nombre", "Mundo")
+    return 'Hola, {nombre}'.format(nombre=escape(nombre))
+
+@app.route('/despedida')
+
+def despedida(name="Carlos"):
+    
+    return 'Hasta la proxima, ' + name
+
+def main():
+ app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+ main()
